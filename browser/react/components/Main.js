09f92d86deb72897aec0a4001a2302deb5a8edc3
Nadim Uddin <nadim.uddin1995@gmail.com>
2017-10-09 18:32:22 -0400

    Almost done

---
+++ browser/react/components/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import AllAlbums from './AllAlbums';
+import SingleAlbum from './SingleAlbum';
+import Sidebar from './Sidebar';
+import Player from './Player';
+import { HashRouter, Route } from 'react-router-dom';
+
+export default class Main extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <HashRouter>
+        <div id="main" className="container-fluid">
+          <div className="col-xs-2">
+            <Sidebar deselectAlbum={this.deselectAlbum} />
+          </div>
+          <div className="col-xs-10">
+            
+              <div className="col-xs-10">
+                <Route exact path="/" component={AllAlbums} />
+                <Route exact path="/albums" component={AllAlbums} />
+                <Route path="/albums/:albumId" component={SingleAlbum} />
+              </div>
+            
+          </div>
+          <Player />
+        </div>
+      </HashRouter>
+    );
+  }
+}
---
+++ browser/react/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = (props) => {
+
+  const deselectAlbum = props.deselectAlbum;
+
+  return (
+    <sidebar>
+      <img src="juke.svg" className="logo" />
+      <section>
+        <h4 className="menu-item active">
+          <Link to="/albums">ALBUMS</Link>
+        </h4>
+      </section>
+    </sidebar>
+  );
+}
+
+export default Sidebar;
